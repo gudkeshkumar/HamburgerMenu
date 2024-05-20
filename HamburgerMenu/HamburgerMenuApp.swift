b7be7650d9183cb708de268793af179b3730f04a
gudkeshkumar <yaduvanshi.gudkesh@gmail.com>
2024-05-20 22:25:34 +0530

    Initial Commit

---
+++ HamburgerMenu/HamburgerMenuApp.swift
@@ -0,0 +1,17 @@
+//
+//  HamburgerMenuApp.swift
+//  HamburgerMenu
+//
+//  Created by kaushal kumar on 20/05/24.
+//
+
+import SwiftUI
+
+@main
+struct HamburgerMenuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
